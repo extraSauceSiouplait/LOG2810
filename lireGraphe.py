fbd2761941248f765fbf769160ca7406d998b034
Alexandre Boudreault <alexandreboudreault@Sansfil-Eduroam-Lassonde-223-72.polymtl.ca>
2017-10-19 17:11:59 -0400

    Added dictionnary

--- lireGraphe.py
+++ lireGraphe.py
@@ -1,29 +1,39 @@
 import numpy
-
-def createGraph(str):
+#from nested_dict import nested_dict
+def creerGraphe(str):
 
     data = open("./" + str, "r")
-    chargingStations = []
-    weights = []
+    chargingStationsDict = {}
+    weightsDict = {} #nested_dict()
 
     for line in data:
         temp = line.strip('\n').split(",")
 
         if len(temp) == 2:
-            temp = [int(x) for x in temp]
-            chargingStations.append(temp)
+			chargingStationsDict[temp[0]] = temp[1]
+            #temp = [int(x) for x in temp]
+            #chargingStations.append(temp)
 
         elif len(temp) == 3:
-            temp = [int(x) for x in temp]
-            weights.append(temp)
+			if (not weightsDict.has_key(temp[0])):
+				weightsDict[temp[0]] = {}
+			weightsDict[temp[0]][temp[1]] = temp[2]
 
-    weightsGraph = numpy.zeros(shape=(len(chargingStations), len(chargingStations)))
+			if (not weightsDict.has_key(temp[1])):
+				weightsDict[temp[1]] = {}
+			weightsDict[temp[1]][temp[0]] = temp[2]
+				
+            #temp = [int(x) for x in temp]
+            #weights.append(temp)
 
-    for arc in weights: 
-		weightsGraph[arc[0]-1,arc[1]-1] = arc[2]
-		weightsGraph[arc[1]-1,arc[0]-1] = arc[2]
+    #weightsGraph = numpy.zeros(shape=(len(chargingStations), len(chargingStations)))
+
+    #for arc in weights: 
+		#weightsGraph[arc[0]-1,arc[1]-1] = arc[2]
+		#weightsGraph[arc[1]-1,arc[0]-1] = arc[2]
 		
-    return weightsGraph
+	#weightsGraph[weightsGraph==0] = -1
+    return (weightsDict,chargingStationsDict)
 
 def convertPointToCity(p):
 	if(p>19 or p<1):
@@ -47,19 +57,33 @@ def convertPointToCity(p):
 				"Verdun",
 				"Ville-Marie",
 				"Villeray-Saint-Michel-Parc-Extension"]
-	return cityNames[p-1]
+	return cityNames[int(p)-1]
 
-def displayGraph(graph):
-	for j in range(graph.shape[0]):
-		neighbors = "(" + str(convertPointToCity(j+1)) + ", " + str(j+1) + ", ("
-		for i in range(graph.shape[0]):
-			if (graph[j,i] != 0):
-				neighbors = neighbors + "(" + str(convertPointToCity(i+1)) + ", " + str(graph[j,i]) + "min)"
+def lireGraphe(graph):
+	for vertex in graph:
+		neighbors = "(" + vertex + ", " + vertex + ", ("
+		for end in graph[vertex]:
+			neighbors = neighbors + "(" + end + ", " + graph[vertex][end] + "mins),"
+
+		neighbors = neighbors[:-1]
 		neighbors = neighbors + ")"
 		print(neighbors)
-	print("done")
+		
+	#for j in range(graph.shape[0]):
+		#neighbors = "(" + str(convertPointToCity(j+1)) + ", " + str(j+1) + ", ("
+		#for i in range(graph.shape[0]):
+			#if (graph[j,i] != 0):
+				#neighbors = neighbors + "(" + str(convertPointToCity(i+1)) + ", " + str(graph[j,i]) + "min)"
+		#neighbors = neighbors + ")"
+		#print(neighbors)
+	#print("done")
+
+#def dijkstra(adjacenceList, start ,end):
+	#distances = 
+	
 
 
-g = createGraph("arrondissements.txt")
-displayGraph(g)
+
+weights,chargingStations = creerGraphe("arrondissements.txt")
+lireGraphe(weights)
 
