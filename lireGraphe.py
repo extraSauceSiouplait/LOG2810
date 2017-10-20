25a5f8a96e70b3253d8fdce9d5c87f2d7d588f2d
Unknown <mbelanger.poly@gmail.com>
2017-10-20 12:46:33 -0400

    Dictionnaire fonctionnel et début djikstra

--- lireGraphe.py
+++ lireGraphe.py
@@ -1,88 +1,100 @@
 import numpy
-#from nested_dict import nested_dict
+
 def creerGraphe(str):
 
     data = open("./" + str, "r")
     chargingStationsDict = {}
-    weightsDict = {} #nested_dict()
+    weightsDict = {}
+
 
     for line in data:
-        temp = line.strip('\n').split(",")
+        temp = line.strip('\n').strip('\r').split(",")
 
         if len(temp) == 2:
-			chargingStationsDict[temp[0]] = temp[1]
-            #temp = [int(x) for x in temp]
-            #chargingStations.append(temp)
+            for i in range(len(temp)):
+                temp[i] = int(temp[i])
+
+            chargingStationsDict[temp[0]] = temp[1]
 
         elif len(temp) == 3:
-			if (not weightsDict.has_key(temp[0])):
-				weightsDict[temp[0]] = {}
-			weightsDict[temp[0]][temp[1]] = temp[2]
+            for i in range(len(temp)):
+                temp[i] = int(temp[i])
 
-			if (not weightsDict.has_key(temp[1])):
-				weightsDict[temp[1]] = {}
-			weightsDict[temp[1]][temp[0]] = temp[2]
-				
-            #temp = [int(x) for x in temp]
-            #weights.append(temp)
+            if (not weightsDict.has_key(temp[0])):
+                weightsDict[temp[0]] = {}
+            weightsDict[temp[0]][temp[1]] = temp[2]
 
-    #weightsGraph = numpy.zeros(shape=(len(chargingStations), len(chargingStations)))
+            if (not weightsDict.has_key(temp[1])):
+                weightsDict[temp[1]] = {}
+            weightsDict[temp[1]][temp[0]] = temp[2]
 
-    #for arc in weights: 
-		#weightsGraph[arc[0]-1,arc[1]-1] = arc[2]
-		#weightsGraph[arc[1]-1,arc[0]-1] = arc[2]
-		
-	#weightsGraph[weightsGraph==0] = -1
     return (weightsDict,chargingStationsDict)
 
 def convertPointToCity(p):
-	if(p>19 or p<1):
-		return "not in MTL"
-	cityNames = ["Ahuntsic-Cartierville",
-				"Anjou",
-				"Cote-des-Neiges-Notre-Dame-de-Grace",
-				"Lachine",
-				"LaSalle",
-				"Le Plateau Mont-Royal",
-				"Le Sud-Ouest",
-				"L'Ile-Bizard-Sainte-Genevieve",
-				"Mercier-Hochelaga-Maisonneuve",
-				"Montreal-Nord",
-				"Outremont",
-				"Pierrefonds-Roxboro",
-				"Riviere-des-Prairies-Pointe-aux-Trembles",
-				"Rosemont-La-Petite-Patrie",
-				"Saint-Laurent",
-				"Saint-Leonard",
-				"Verdun",
-				"Ville-Marie",
-				"Villeray-Saint-Michel-Parc-Extension"]
-	return cityNames[int(p)-1]
+    if(p>19 or p<1):
+        return "not in MTL"
+    cityNames = ["Ahuntsic-Cartierville",
+                "Anjou",
+                "Cote-des-Neiges-Notre-Dame-de-Grace",
+                "Lachine",
+                "LaSalle",
+                "Le Plateau Mont-Royal",
+                "Le Sud-Ouest",
+                "L'Ile-Bizard-Sainte-Genevieve",
+                "Mercier-Hochelaga-Maisonneuve",
+                "Montreal-Nord",
+                "Outremont",
+                "Pierrefonds-Roxboro",
+                "Riviere-des-Prairies-Pointe-aux-Trembles",
+                "Rosemont-La-Petite-Patrie",
+                "Saint-Laurent",
+                "Saint-Leonard",
+                "Verdun",
+                "Ville-Marie",
+                "Villeray-Saint-Michel-Parc-Extension"]
+    return cityNames[int(p)-1]
 
 def lireGraphe(graph):
-	for vertex in graph:
-		neighbors = "(" + vertex + ", " + vertex + ", ("
-		for end in graph[vertex]:
-			neighbors = neighbors + "(" + end + ", " + graph[vertex][end] + "mins),"
+    for startingVertex, destination in sorted(graph.iteritems()):
 
-		neighbors = neighbors[:-1]
-		neighbors = neighbors + ")"
-		print(neighbors)
-		
-	#for j in range(graph.shape[0]):
-		#neighbors = "(" + str(convertPointToCity(j+1)) + ", " + str(j+1) + ", ("
-		#for i in range(graph.shape[0]):
-			#if (graph[j,i] != 0):
-				#neighbors = neighbors + "(" + str(convertPointToCity(i+1)) + ", " + str(graph[j,i]) + "min)"
-		#neighbors = neighbors + ")"
-		#print(neighbors)
-	#print("done")
+        neighbors = "(" + str(startingVertex) + ", " + str(startingVertex) + ", ("
 
-#def dijkstra(adjacenceList, start ,end):
-	#distances = 
-	
+        for destinationVertex, distance in sorted(destination.iteritems()):
+            neighbors = neighbors + "(" + str(destinationVertex) + ", " + str(distance) + "mins),"
+predecessors = {}
+        neighbors = neighbors[:-1]
+        neighbors = neighbors + ")"
+        print(neighbors)
 
 
+def dijkstra(adjacenceList, start, end, Visited = [], distances = {}, predecessors = {}):
+
+
+    if start not in adjacenceList:
+        raise TypeError("Le point de départ n'est pas présent pas dans la carte")
+    if end not in adjacenceList:
+        raise  TypeError("Le point d'arrivée n'est pas présent dans la carte")
+
+    #Initialise les coûts (infini pour les
+    #sommets non visités
+    for vertex in adjacenceList:
+        if vertex != start:
+            distances[vertex] = float('inf')
+            predecessors[vertex] = Null
+            notVisited.append(vertex)
+
+    distances[start] = 0
+
+        min(vertex for vertex in distances if vertex not in visited, key= distances.get())
+
+            distances[start] = 0
+
+        for voisin in adjacenceList.items():
+            newDistance = distances[]
+
+    #Condition finale si on a trouvé le chemin jusqu'a end
+    #On construit le chemin le plus court
+    if start == end:
 
 weights,chargingStations = creerGraphe("arrondissements.txt")
 lireGraphe(weights)
